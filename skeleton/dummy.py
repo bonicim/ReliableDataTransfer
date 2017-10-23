794b0a7f145c54453a2605f26b470c1369c15c1b
Mark Bonicillo <markabonicillo@gmail.com>
2017-10-23 23:46:18 +0100

    Updated code style to reflect 4 spaces for indentation.

--- skeleton/dummy.py
+++ skeleton/dummy.py
@@ -6,23 +6,23 @@ import udt
 # messages from the underlining network layer without any check on
 # loss or bit errors.
 class DummyTransportLayer:
-  # "msg_handler" is used to deliver messages to application layer
-  # when it's ready.
-  def __init__(self, local_port, remote_port, msg_handler):
-    self.network_layer = udt.NetworkLayer(local_port, remote_port, self)
-    self.msg_handler = msg_handler
+	# "msg_handler" is used to deliver messages to application layer
+	# when it's ready.
+	def __init__(self, local_port, remote_port, msg_handler):
+		self.network_layer = udt.NetworkLayer(local_port, remote_port, self)
+		self.msg_handler = msg_handler
 
-  # "send" is called by application. Return true on success, false
-  # otherwise.
-  def send(self, msg):
-    self.network_layer.send(msg)
-    return True
+	# "send" is called by application. Return true on success, false
+	# otherwise.
+	def send(self, msg):
+		self.network_layer.send(msg)
+		return True
 
-  # "handler" to be called by network layer when packet is ready.
-  def handle_arrival_msg(self):
-    msg = self.network_layer.recv()
-    self.msg_handler(msg)
+	# "handler" to be called by network layer when packet is ready.
+	def handle_arrival_msg(self):
+		msg = self.network_layer.recv()
+		self.msg_handler(msg)
 
-  # Cleanup resources.
-  def shutdown(self):
-    self.network_layer.shutdown()
+	# Cleanup resources.
+	def shutdown(self):
+		self.network_layer.shutdown()
--- skeleton/file_receiver.py
+++ skeleton/file_receiver.py
@@ -8,30 +8,30 @@ import util
 
 
 def msg_handler(file_handle, msg):
-  file_handle.write(msg)
+	file_handle.write(msg)
 
 
 if __name__ == '__main__':
-  if len(sys.argv) != 3:
-    print('Usage: python file_receiver.py [dummy|ss|gbn] [file_name]')
-    sys.exit(1)
+	if len(sys.argv) != 3:
+		print('Usage: python file_receiver.py [dummy|ss|gbn] [file_name]')
+		sys.exit(1)
 
-  transport_layer = None
-  transport_layer_name = sys.argv[1]
-  file_name = sys.argv[2]
-  assert not os.path.exists(file_name)
-  file_handle = None
-  try:
-    file_handle = open(file_name, 'w')
-    transport_layer = util.get_transport_layer_by_name(
-        transport_layer_name,
-        config.RECEIVER_LISTEN_PORT,
-        config.SENDER_LISTEN_PORT,
-        functools.partial(msg_handler, file_handle))
-    while True:
-      time.sleep(1)
-  finally:
-    if file_handle:
-      file_handle.close()
-    if transport_layer:
-      transport_layer.shutdown()
+	transport_layer = None
+	transport_layer_name = sys.argv[1]
+	file_name = sys.argv[2]
+	assert not os.path.exists(file_name)
+	file_handle = None
+	try:
+		file_handle = open(file_name, 'w')
+		transport_layer = util.get_transport_layer_by_name(
+			transport_layer_name,
+			config.RECEIVER_LISTEN_PORT,
+			config.SENDER_LISTEN_PORT,
+			functools.partial(msg_handler, file_handle))
+		while True:
+			time.sleep(1)
+	finally:
+		if file_handle:
+			file_handle.close()
+		if transport_layer:
+			transport_layer.shutdown()
